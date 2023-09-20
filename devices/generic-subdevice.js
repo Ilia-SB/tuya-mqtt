e4ba8cc267f0a07ebad935c9c68c9d7ec6716457
Ilia Ivanov <ilia.a.ivanov@gmail.com>
2023-09-20 13:38:16 +0300

    More debug

--- devices/generic-subdevice.js
+++ devices/generic-subdevice.js
@@ -41,6 +41,7 @@ class GenericSubDevice extends TuyaDevice {
     }
 
     init() {
+        debug('Init')
         debug('Generic ' + this.isPassive ? 'passive ' : '' + 'passive subdevice init() for ' + this.toString())
         this.deviceData.mdl = 'Generic Subdevice'
 
